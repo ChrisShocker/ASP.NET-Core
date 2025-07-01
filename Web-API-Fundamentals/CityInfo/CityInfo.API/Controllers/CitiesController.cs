5b5c2a841f7cfc3e96fb702a60084aa4266ee856
Chris Andersen <chris_andersen@outlook.com>
2025-07-01 08:55:44 -0700

    add controller and route attributes to use it

---
+++ Web-API-Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    //use APIController attribute to add support for automatic model validation, binding source inference, and more
+    [ApiController]
+
+    /*
+     * specify the base route for this controller
+     * [Route<"api/[controller]">] can be used to
+     * dynamically set the controller name in the route
+    */
+    [Route("api/cities")] 
+    public class CitiesController : ControllerBase
+    {
+        // use routing attribute to specify the route for this controller
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            // this is a simple example of returning JSON data
+            return new JsonResult(new[]
+            {
+                new { Id = 1, Name = "New York" },
+                new { Id = 2, Name = "Los Angeles" },
+                new { Id = 3, Name = "Chicago" }
+            });
+        }
+    }
+}
--- Web-API-Fundamentals/CityInfo/CityInfo.API/Program.cs
+++ Web-API-Fundamentals/CityInfo/CityInfo.API/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -13,8 +12,12 @@ var app = builder.Build();
 /* Everything below is middleware and the order it's declared in matters */
 
 // Configure the HTTP request pipeline.
+// this is the default env setup, other envs can be used to configure different middleware
 if (app.Environment.IsDevelopment())
 {
+    // only add the following middleware when the app environment is development
+
+    // this will add the swagger generator to the app
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -22,8 +25,11 @@ if (app.Environment.IsDevelopment())
 // redirect http calls to be https
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
+// No routes are specefied, so we have to setup the routing manually using attribute based routing 
 app.MapControllers();
 
 app.Run();

16fc7654990d2eed93f3b6e1db8773389f9f7e74
Chris Andersen <chris_andersen@outlook.com>
2025-07-01 10:42:58 -0700

    add cities store to be used by the api

---
+++ Web-API-Fundamentals/CityInfo/CityInfo.API/Cities.cs
@@ -0,0 +1,42 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        // static list of cities that used as a data store
+        public List<CityDto> Cities { get; set; }
+
+        //add current data store as a singleton so it can be used throughout the app
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        // constructor to initialize the data store with some sample cities
+        public CitiesDataStore()
+        {
+            // initialize the Cities list with some sample data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York",
+                    Description = "The Big Apple"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Los Angeles",
+                    Description = "The City of Angels"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Chicago",
+                    Description = "The Windy City"
+                }
+
+
+            };
+        }
+    }
+}

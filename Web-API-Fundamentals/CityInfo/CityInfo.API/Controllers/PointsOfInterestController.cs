090adb6d7eb2b88ec6761a3c586b106e147eceef
Chris Andersen <chris_andersen@outlook.com>
2025-07-02 09:54:37 -0700

    Add CreatePointOfInterest endpoint and related DTO
    Updated PointsOfInterestController to include a new CreatePointOfInterest endpoint, enabling the creation of points of interest for a specified city. The endpoint validates city existence, assigns a unique ID, and returns a CreatedAtRoute response using the GetPointOfInterest route.
    
    Added PointOfInterestForCreationDto in CityInfo.API.Models to serve as a DTO for creating new points of interest, with required Name and optional Description properties.

--- Web-API-Fundamentals/CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ Web-API-Fundamentals/CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -20,7 +20,8 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointsOfInterestId}")]
+        // add a name so this endpoint can be referenced in other endpoints
+        [HttpGet("{pointsOfInterestId}", Name = "GetPointOfInterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(
             int cityId,
             int pointsOfInterestId
@@ -41,6 +42,38 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(
+            int cityId,
+            PointOfInterestForCreationDto pointOfInterest
+        )
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore
+                .Current.Cities.SelectMany(c => c.PointsOfInterest)
+                .Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description,
+            };
+
+            //return uri of where the new point of interest can be found
+            return CreatedAtRoute(
+                "GetPointOfInterest",
+                new { cityId = cityId, pointsOfInterestId = finalPointOfInterest.Id },
+                finalPointOfInterest
+            );
         }
     }
 }
